907e79f5a1140819eb550f914bfd2aee9d819066
SeleneHdz01 <hdz_f@hotmail.com>
2019-05-27 17:53:56 -0500

    Modificando package.json

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,20 +1,24 @@
 import React from "react";
 
 const Home = () => (
-    <div>
-        <h1>Burger Queen</h1>
+  <div>
+    <h1>Burger Queen</h1>
 
-        <div className="container">
-            <div className="row">
-                <div className="col">
-                    <a href='/order' className='btn btn-info'>Mesero</a>
-                </div>
-                <div className="col">
-                <a href='/kitchen' className='btn btn-primary'>Cocina</a>
-                </div>
-            </div>
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <a href="/order" className="btn btn-info">
+            Mesero
+          </a>
         </div>
+        <div className="col">
+          <a href="/kitchen" className="btn btn-info">
+            Cocina
+          </a>
+        </div>
+      </div>
     </div>
+  </div>
 );
-    
+
 export default Home;
